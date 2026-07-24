c6b84c5142cedbdad3211c17ea4b88ba8773bb71
haoyu6 <153610481@qq.com>
2026-07-24 17:44:01 +0800

    fix: 修复局部修改保存问题

--- packages/app/octoapp/context/dialog-iframe.tsx
+++ packages/app/octoapp/context/dialog-iframe.tsx
@@ -1,2 +1,9 @@
 // jk-j60099994-replace-with-60062650-dialog-iframe-1-start
+    export function useDialogIframe () {
+        return {
+            show: (cb: (data: string) => {}) => {
+                cb(JSON.stringify({a: 1}))
+            }
+        }
+    }
 // jk-j60099994-replace-with-60062650-dialog-iframe-1-end
