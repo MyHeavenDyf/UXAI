4b030968bcd2a1d602d1945e544bac66c198550f
ader-h <1059803085@qq.com>
2026-08-05 18:54:44 +0800

    去除title-bar 的标记

--- packages/app/octoapp/pages/pattern/modules/preview/title-bar.tsx
+++ packages/app/octoapp/pages/pattern/modules/preview/title-bar.tsx
@@ -255,7 +255,6 @@ export function TitleBar(props: TitleBarProps) {
           </button>
 
           {/* 按钮：画布编辑 */}
-          // jk-j60099994-replace-with-wx372822-pattern-topixso-3-start
           {/* <button
             class="pattern-action-btn"
             title="画布编辑"
@@ -264,7 +263,6 @@ export function TitleBar(props: TitleBarProps) {
             <IconActionCanvasEdit size={16} />
             <span>画布编辑</span>
           </button> */}
-          // jk-j60099994-replace-with-wx372822-pattern-topixso-3-end
 
           {/* 按钮 4：历史版本 */}
           <div class="dropdown-trigger-container">
@@ -341,7 +339,7 @@ export function TitleBar(props: TitleBarProps) {
           </button>
 
           {/* 按钮 8：归档 */}
-          {/* <button
+          <button
             class="pattern-action-btn"
             classList={{ 'edit-active': !!props.archiving }}
             title="归档"
@@ -349,7 +347,7 @@ export function TitleBar(props: TitleBarProps) {
           >
             <IconActionDownload size={16} />
             <span>归档</span>
-          </button> */}
+          </button>
         </div>
       </div>
     </div>
