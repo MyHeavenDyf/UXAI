32ffe9be12fac9114a8e909c37c973a58f8fabf8
xiaomengqiang <610800236@qq.com>
2026-08-15 17:47:33 +0800

    更改previewdist的图片的路径

--- packages/previewdist/data.js
+++ packages/previewdist/data.js
@@ -40,7 +40,7 @@ window.__A2UI_DATA__ = {
         ],
         "ghUserInfo": {
             "userName": "Admin",
-            "avatarImage": "https://randomuser.me/api/portraits/men/32.jpg"
+            "avatarImage": "./uploads/image.jpg"
         },
         "lrlCardTitle": "链路结果",
         "lrlSearchCategory": "请选择",
